c8593da2e52b012266df7c5505bddc7da2074470
teotimepacreau <teotime.pac@outlook.fr>
2024-07-25 21:49:45 +0200

    form filling

--- bot.js
+++ bot.js
@@ -5,13 +5,18 @@ function sleep(ms) {
      })
 }
 (async ()=> {
-    const browser = await puppeteer.launch({headless: false})
+    const browser = await puppeteer.launch({headless: false})// SI ON PAS MET HEADLESS FALSE ON NE VERRA PAS LE NAVIGATEUR S'OUVRIR ET EXÉCUTER  
     const page = await browser.newPage()
     await page.goto('https://eservices.nantesmetropole.fr/web/guest/accueil-particuliers')
 
 
     await page.click('#sign-in')
-    await sleep(5000)
+    await sleep(2000)
+
+    await page.type("[name='user']", process.env.IDENTIFIANT)
+    await page.type("[name='password']", process.env.PASSWORD)
+    await page.click("#submitLDAP")
+    await sleep(6000)
 
     await browser.close()
 })()
