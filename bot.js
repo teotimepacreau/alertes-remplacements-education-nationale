1435657cf522b2788397e4058187e984f184a679
Teotime Pacreau <teotime.pac@outlook.fr>
2024-08-04 17:55:00 +0200

    mediatheque

--- bot.js
+++ bot.js
@@ -8,14 +8,22 @@ function sleep(ms) {
     const browser = await puppeteer.launch({
         headless: false, // SI ON PAS MET HEADLESS FALSE ON NE VERRA PAS LE NAVIGATEUR S'OUVRIR ET EXÉCUTER 
         slowMo: 50,
-        devtools: true,
-    }) 
+    })
     const page = await browser.newPage()
     await page.goto('https://catalogue-bibliotheque.nantes.fr/account#')
 
 
     await page.locator("#loginField").fill(process.env.IDENTIFIANT)
     await page.locator("#passwordField").fill(process.env.PASSWORD)
+    
     await page.locator('main form button:nth-child(3)').click()
+    
+    await page.locator('#app > div > div:nth-child(3) > div > div > div > div > div > div > div > div._c8-1 > div > div._0-0 > div > div._c9-0 > main > div > div > div > div > ul > div:nth-child(4) > div:nth-child(2) > div').click()
+    
+    let livresReservees = await page.$$eval('#searchresult > *', element => {
+        return element.querySelector('span.jss902')
+    })
+    console.log(livresReservees)
+    
     // await browser.close()
 })()
